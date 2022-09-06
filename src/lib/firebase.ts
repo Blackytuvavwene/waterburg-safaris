5bcbda4ad59a778bf17ada011bb3fb1a9ee06a67
Boitumelo Tubabwene <btexblacky@gmail.com>
2022-09-06 10:50:37 +0200

    changed env vars

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { collection, doc, getDoc, getDocs, getFirestore} from "firebase/firestore";
 import { aboutCompanyConverter } from "./app-components/about-components/about.types";
-import { 
-    FIREBASE_API_KEY, 
-    FIREBASE_APP_ID, 
-    FIREBASE_AUTH_DOMAIN, 
-    FIREBASE_MEASURING_ID, 
-    FIREBASE_MESSENGER_ID, 
-    FIREBASE_PROJECT_ID, 
-    FIREBASE_STORAGE_BUCKET } from "$env/static/private";
+// import { 
+//     FIREBASE_API_KEY, 
+//     FIREBASE_APP_ID, 
+//     FIREBASE_AUTH_DOMAIN, 
+//     FIREBASE_MEASURING_ID, 
+//     FIREBASE_MESSENGER_ID, 
+//     FIREBASE_PROJECT_ID, 
+//     FIREBASE_STORAGE_BUCKET } from "$env/static/private";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,13 +26,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSENGER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASURING_ID
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSENGER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASURING_ID
 };
 
 // Initialize Firebase
