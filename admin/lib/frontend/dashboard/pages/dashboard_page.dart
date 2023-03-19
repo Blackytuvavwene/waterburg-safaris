afef911c3b4aba77e8d1066006774761d7006108
Boitumelo Tubabwene <btexblacky@gmail.com>
2023-03-19 16:27:51 +0200

    updated dashboard

--- admin/lib/frontend/dashboard/pages/dashboard_page.dart
+++ admin/lib/frontend/dashboard/pages/dashboard_page.dart
@@ -284,7 +284,7 @@ class _DesktopDashboardPage extends HookConsumerWidget {
         child: ListView(
           children: [
             activitiesWidget!,
-            bookingsWidget!,
+            bookingsWidget!, 
           ],
         ),
       ),
